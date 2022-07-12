dfeb5558bb4696e26ef9ff246c97bd97aed7fee3
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-12 13:56:08 +0530

    tailwind ui pending

---
+++ index.js
@@ -0,0 +1,10 @@
+import { products } from "./src/js/products.js";
+
+/* This is a function that is listening for the DOM to be ready. */
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
+});
+
+const initApp = () => {
+  // products();
+};
